6d6877518e0ac72c6dad6d41bea702ffced86c4d
Yasha Lüscher <Yasha.luescher@intergga.ch>
2026-05-10 16:55:38 +0200

    added zurueck button on levels scene

--- scenes/levelAuswahl.gd
+++ scenes/levelAuswahl.gd
@@ -17,3 +17,7 @@ func _on_texture_button_pressed() -> void:
 
 func _on_texture_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Scene2.tscn")
+
+
+func _on_zurueck_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/StartScreen.tscn")
